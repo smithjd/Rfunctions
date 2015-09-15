e4a3342c1d5816e038b40272c4a5e5206eacf053
John David Smith <john.smith@learningalliances.net>
2015-09-15 15:28:56 -0700

    add line to make nice titles.

--- setup_my_google.R
+++ setup_my_google.R
@@ -2,10 +2,13 @@
 #
 # Call this once to set up methods to easily upload data frames to a Google Spreadsheet.
 #
+# may need:  devtools::install_github("jennybc/googlesheets")
+library(dplyr)
+library(lazyeval)
 library(googlesheets)
 library(httr)
 
-# my_google <- oauth_app("google", key = Sys.getenv("jd8_oauth_id"), secret = Sys.getenv("jd8_oauth_id" ))
+# my_google <- oauth_app("google", key = Sys.getenv("jd8_oauth_id"), secret = Sys.getenv("jd8_oauth_secret" ))
 
 my_google <- oauth_app("google", key = Sys.getenv("jds_shambhala_id"), secret = Sys.getenv("jds_shambhala_secret" ))
 
@@ -16,12 +19,14 @@ google_token <- oauth2.0_token(oauth_endpoints("google"), my_google,
 #
 # assumes that you've set up the environment by calling setup_my_google.R
 #
-upload_google_ss <- function(df_name) {
+upload_google_ss <- function(df_name, title = "NONE") {
   if (is.data.frame(df_name)) {
     df_name_quote_csv <- paste(deparse(substitute(df_name)),".csv", sep = "")
+    names(df_name) <-  gsub("[_.]"," ", names(df_name))
     write.csv(df_name, file = df_name_quote_csv, row.names = F) # create local copy
-    my_ss <- upload_ss(df_name_quote_csv) # local copy
-    my_ss
+    sheet_title <- ifelse(title == "NONE", df_name, title)
+    my_ss <- gs_upload(df_name_quote_csv, sheet_title = sheet_title) # local copy
     file.remove(df_name_quote_csv) # local copy
+    my_ss
   }
 }
