6b4559a0b2654ec11f77a67dda12b17eda5649a9
John David Smith <john.smith@learningalliances.net>
2017-11-06 13:08:05 -0800

    cumulating many updates and improvements.

--- make_dd.R
+++ make_dd.R
@@ -8,15 +8,16 @@
 
 parse_str_out <- function(str_output) {
   require(stringr)
-  str_words <- str_extract_all(str_output, "\\w+")[[1]][1:2]
-  var_name <- str_words[1]
-  var_format <- str_words[2]
+  str_output <- str_replace_all(str_output, ":", " :")
+  var_name <- word(str_output, 3)
+  var_format <- word(str_output, 5, sep = " +")
   df_out <- tibble(var_name, var_format)
   df_out
 }
 
 str_to_df <- function(df) {
-  str_out <- capture.output(str(df))
+  str_out <- capture.output(str(df, list.len = ncol(df)))
+  # str_out <- capture.output(str(df))
   # str(str_out)
   str_as_df <- map_df(str_out, parse_str_out)
   str_as_df[2:length(str_out),]
@@ -29,16 +30,27 @@ fivenumsum <- function(x){
   require(tidyverse)
   require(lubridate)
   n_rows <- length(x)
+  # handle SQL "missing value" for dates...
+  x <- ifelse(is.character(x) &
+                (x == "0000-00-00 00:00:00" | x == "0000-00-00"), "", x)
+  x <- parse_guess(x)
   non_blanks <- x[complete.cases(x)]
   non_missing_n <- length(non_blanks)
   if (non_missing_n == 0) {
     summary <- tibble(n_rows = n_rows, num_blank = n_rows, num_unique = 0,
                       min = "", q_25 = "", q_50 = "", q_75 = "", max = "")
     return(summary)
-  } else {
-    num_blank <- ifelse(is.POSIXct(x),
-                        sum(is.na(x)),
-                        sum(x == ""))
+  } else if (non_missing_n == 1) {
+    summary <- tibble(n_rows = n_rows, num_blank = n_rows, num_unique = 0,
+                      min = "", q_25 = "", q_50 = "", q_75 = "", max = "")
+    summary$q_50 = as.character(non_blanks[1])
+    return(summary)
+  }
+    else {
+    # num_blank <- ifelse(is.Date(x),
+    #                     sum(is.na(x)),
+    #                     sum(x == ""))
+    num_blank <- sum(is.na(x))
     num_unique <- length(unique(non_blanks))
     med_pos <- round((max(1,non_missing_n/2)),0)
     qrt_pos <- round((max(1,non_missing_n/4)),0)
@@ -59,9 +71,10 @@ fivenumsum <- function(x){
   }
 }
 
-make_dd <- function(df){
+make_dd <- function(df, df_alias = NULL){
   require(tidyverse)
   df_name <- substitute(df)
+  if(!is.na(df_alias)){df_name <- df_alias}
   df_var_nums <- dim(df)[2]
   fivesum <- map_df(df,fivenumsum)
   snames <- as.character(names(df))
